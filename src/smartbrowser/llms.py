3c7a6a4a587f5c4378040dc8bbf8047aa55090b9
VianneyMI <vianney.mixtur@outlook.fr>
2025-03-10 23:40:09 +0100

    Add TODO note to remember to handle more models

--- src/smartbrowser/llms.py
+++ src/smartbrowser/llms.py
@@ -1,6 +1,5 @@
 """Interface to LLM providers."""
 
-
 from typing import Literal, get_args
 
 from langchain.chat_models.base import BaseChatModel
@@ -9,19 +8,17 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
 
-
-google_models_literal = Literal["gemini-2.0-flash"] # to be completed
-anthropic_models_literal = Literal["claude-3-5-sonnet-latest"] # to be completed
-openai_models_literal = Literal["gpt-4o-mini"] # to be completed
-ollama_models_literal = Literal["llama3.2:latest"] # to be completed
+# TODO : Complete the list of the models.
+google_models_literal = Literal["gemini-2.0-flash"]  # to be completed
+anthropic_models_literal = Literal["claude-3-5-sonnet-latest"]  # to be completed
+openai_models_literal = Literal["gpt-4o-mini"]  # to be completed
+ollama_models_literal = Literal["llama3.2:latest"]  # to be completed
 all_models_literal = Literal[
-    "gemini-2.0-flash",
-    "claude-3-5-sonnet-latest",
-    "gpt-4o-mini",
-    "llama3.2:latest"
-                             ]
+    "gemini-2.0-flash", "claude-3-5-sonnet-latest", "gpt-4o-mini", "llama3.2:latest"
+]
 
-def get_llm(model_name:all_models_literal)->BaseChatModel:
+
+def get_llm(model_name: all_models_literal) -> BaseChatModel:
     """Returns the appropriate ChatModel based on the model name."""
 
     if model_name in get_args(google_models_literal):
@@ -34,4 +31,3 @@ def get_llm(model_name:all_models_literal)->BaseChatModel:
         return ChatOllama(model=model_name)
     else:
         raise ValueError(f"Model {model_name} not supported.")
-        
